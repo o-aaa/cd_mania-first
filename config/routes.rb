4b0379ede16cbfbc92b50fcd82f565df2f6974f8
satenmenf <nf.satomi1gou@gmail.com>
2019-05-15 15:41:09 +0900

    [Add]controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/confirmation'
+  get 'order_items/index'
+  get 'favorites/index'
+  get 'addresses/new'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
+  get 'products/index'
+  get 'products/show'
+  get 'products/new'
+  get 'products/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
