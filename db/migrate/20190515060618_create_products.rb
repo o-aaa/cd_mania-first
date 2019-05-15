772175c2d81b9f5185c87a0b2849b2b0ac0eb2e4
satenmenf <nf.satomi1gou@gmail.com>
2019-05-15 15:14:30 +0900

    [Add]productモデル

---
+++ db/migrate/20190515060618_create_products.rb
@@ -0,0 +1,18 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.integer :product_id
+      t.string :disc_name
+      t.string :artist
+      t.integer :price
+      t.date :release_date
+      t.text :cover_image_id
+      t.string :lebel
+      t.integer :genre, default: 0
+      t.integer :buy_status, default: 0
+      t.integer :stocks
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_060339) do
+ActiveRecord::Schema.define(version: 2019_05_15_060618) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "address_id"
@@ -30,6 +30,21 @@ ActiveRecord::Schema.define(version: 2019_05_15_060339) do
     t.integer "favorite_id"
     t.integer "user_id"
     t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "product_id"
+    t.string "disc_name"
+    t.string "artist"
+    t.integer "price"
+    t.date "release_date"
+    t.text "cover_image_id"
+    t.string "lebel"
+    t.integer "genre", default: 0
+    t.integer "buy_status", default: 0
+    t.integer "stocks"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
