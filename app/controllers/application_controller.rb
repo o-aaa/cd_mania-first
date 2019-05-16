ae731f81fd0ba4f9a54669ca99f0a05467654dc7
satenmenf <nf.satomi1gou@gmail.com>
2019-05-16 17:43:37 +0900

    update sign_up form 新規投稿画面に「名前」「電話番号」の投稿機能を実装しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+	# devise新規登録で追加するデータ
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :phone_num, :user_flag, :user_status ])
+	end
 end
