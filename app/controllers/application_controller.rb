8f9ee293336dda3c3006ac37d7ace9152874cf40
satenmenf <nf.satomi1gou@gmail.com>
2019-05-16 20:31:12 +0900

    add post_num to sign_up view 新規登録画面に郵便番号の入力フォームを追加しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :phone_num, :user_flag, :user_status ])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :phone_num, :user_flag, :user_status, :post_num => [:prefectures_master_id] ])
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get "/users/:id/orders/confirmation/:id" => "orders#confirmation", as: 'confirmation'
   get "/order_items" => "order_items#index", as: 'index'
   get "/thankyou" => "order_items#thankyou", as: 'thankyou'
-  devise_for :users
+  devise_for :users, controllers: {sessions: 'users/sessions'}
   resources :users, only:[:index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
