5617026803b31f161815520208996ff810d1e240
satenmenf <nf.satomi1gou@gmail.com>
2019-05-19 17:58:25 +0900

    update user_info layout マイページのレイアウトを更新しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,9 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-  	@address = Address.find_by(id[:user_id])
+  	@address = Address.find_by(user_id: @user.id)
+  	# ここから購入履歴
+  	@orders = Order.page(params[:page]).reverse_order
   end
 
   def new
