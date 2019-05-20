9f200264915a91ed30defb0f7a9bffdceabfc6a8
satenmenf <nf.satomi1gou@gmail.com>
2019-05-20 16:31:35 +0900

    update mypage layout マイページのレイアウト（tableの余白）を更新しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   	@address = @user.addresses.first
   	# ここから購入履歴
-  	@orders = Order.where(user_id: @user.id)
+  	@orders = Order.where(user_id: @user.id).includes(:order_items) #入れ子型のeach文を作成するため
   end
+  # 次回参考：https://qiita.com/blueplanet/items/05aa424cc7e5847e6c84
 
   def new
   end
