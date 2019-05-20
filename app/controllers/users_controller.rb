9319ca76bf2f4868aa59e08ac1fac3c51b786335
buoriky7 <buoriky7@gmail.com>
2019-05-20 07:39:12 +0000

    first-commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   def index
+  	@users = User.all
+
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to action: 'index'
   end
 
   def show
@@ -10,5 +18,6 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+
 end
- 
