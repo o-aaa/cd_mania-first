8131d98393383cc702ef10ea852f1aef74a776e5
buoriky7 <buoriky7@gmail.com>
2019-05-23 10:25:09 +0000

    add_admin_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,10 @@ class UsersController < ApplicationController
   	@address = @user.addresses.first
   	# ここから購入履歴
   	@orders = Order.where(user_id: @user.id).includes(:order_items) #入れ子型のeach文を作成するため
+    
+
+    @sales = OrderItem.sum(:buy_price)
+    # 該当月の絞り込みを追加予定。現在は全合計のみ
   end
   # 次回参考：https://qiita.com/blueplanet/items/05aa424cc7e5847e6c84
 
