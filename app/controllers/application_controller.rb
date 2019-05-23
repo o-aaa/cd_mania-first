9535e99801410a55df10c4c26a4d2aa94cca15fc
satenmenf <nf.satomi1gou@gmail.com>
2019-05-23 17:35:02 +0900

    update search_form to application_header 検索機能を実装しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
 	# devise新規登録で追加するデータ
 	before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :set_search
 
+
+
+	# 検索フォーム
+	def set_search
+		@search = Product.ransack(params[:q]) #ransackタグで検索機能を実装
+		@search_products = @search.result.page(params[:page]) #結果を返す。kaminariでページャ機能つける
+  	end
+
+
+	# devise用ストロングパラメーター
 	def configure_permitted_parameters
 		# binding.pry
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :phone_num, :user_flag, :user_status, addresses_attributes: [:id, :post_num, :address, :shipping_address] ])
