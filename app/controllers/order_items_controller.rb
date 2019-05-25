198a56c6eadfa043d806d2890531e3401c1389f2
satenmenf <nf.satomi1gou@gmail.com>
2019-05-25 16:12:56 +0900

    update product&cart date  to  order_item_index 注文経歴に、productとcartの情報が表示できるようになりました。

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,8 +1,6 @@
 class OrderItemsController < ApplicationController
   def index
   	@orders = Order.page(params[:page]).includes(:order_items)
-  	# @order = Order.find(1)
-  	# @address = @orders.address
   end
 
   def thankyou
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,7 @@ class Cart < ApplicationRecord
 	# アソシエーション
 	belongs_to :user
 	belongs_to :product
+	has_one :order_item
 
 	# 論理削除(Gem:paranoia)を使用
     acts_as_paranoid
