4dddb67d57032a84f2a08c21e7d7e339545fcc4c
satenmenf <nf.satomi1gou@gmail.com>
2019-05-25 18:17:48 +0900

    update image to order_item_index 注文経歴に、ジャケット画像が表示されるようになりました。

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,6 +1,7 @@
 class OrderItemsController < ApplicationController
   def index
   	@orders = Order.page(params[:page]).includes(:order_items)
+  	@order_item = OrderItem.find(1)
   end
 
   def thankyou
