58b29c2eefd3d57d1f9289381799264397ecaa1d
satenmenf <nf.satomi1gou@gmail.com>
2019-05-25 13:56:38 +0900

    update order_item layout Address, Order, Userのデータを表示

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,6 +1,8 @@
 class OrderItemsController < ApplicationController
   def index
   	@orders = Order.page(params[:page]).includes(:order_items)
+  	# @order = Order.find(1)
+  	# @address = @orders.address
   end
 
   def thankyou
