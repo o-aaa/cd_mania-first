e4fc0a1cd4b53948726f7a893490e605acb57ab9
satenmenf <nf.satomi1gou@gmail.com>
2019-05-28 12:09:34 +0900

    update /user_order_history マイページ画面の注文経歴のレイアウトを更新しました。

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,7 +1,6 @@
 class OrderItemsController < ApplicationController
   def index
   	@orders = Order.page(params[:page]).includes(:order_items)
-  	@order_item = OrderItem.find(1)
   	@order = Order.new
   end
 
