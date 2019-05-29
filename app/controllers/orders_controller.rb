53fe1648dca7ef81248e7d3a7e891645fb696671
satenmenf <nf.satomi1gou@gmail.com>
2019-05-29 11:57:12 +0900

    fix confirmation form 最終確認画面で、カートが物理削除されるエラーを修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -73,7 +73,9 @@ class OrdersController < ApplicationController
     end
     order_items = OrderItem.where(order_id: order.id)
     if order_items.count == carts.count
-      carts.delete_all
+      carts.each do |cart|
+        cart.destroy
+      end
       redirect_to thankyou_path
     else
       flash[:danger] = "カート内容の更新に失敗しました。もう一度やり直してください。"
