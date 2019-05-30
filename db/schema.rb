d0b176895b73ba8819c9561665048aae643e2f49
satenmenf <nf.satomi1gou@gmail.com>
2019-05-30 16:39:28 +0900

    fix remove_column to  Order,Cart 現在のブランチで編集中に作成したカラムのうち、不要なカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_082951) do
+ActiveRecord::Schema.define(version: 2019_05_30_072550) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "post_num"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2019_05_29_082951) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "ordered_flag", default: "0"
   end
 
   create_table "discs", force: :cascade do |t|
@@ -65,7 +64,6 @@ ActiveRecord::Schema.define(version: 2019_05_29_082951) do
     t.integer "address_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "ordered_flag", default: "0"
     t.integer "update_flag", default: 0
   end
 
